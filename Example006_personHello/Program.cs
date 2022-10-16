163ed1c25333def9f9f120b42c8f22460bcb73a1
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-10-16 19:30:10 +0700

    Add if/else condition

---
+++ Example006_personHello/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("EEE, it is Masha!!!");
+}
+else
+{
+    Console.WriteLine("Hi, " + username);
+}
