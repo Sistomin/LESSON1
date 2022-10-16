ecbc9689097e95efda8f030bd96a68f6c5ebc9b1
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-10-16 11:44:55 +0700

    Add example001 HelloConsole projekt!

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.WriteLine("2 Hello, World! Hello, Console?");
 // dotnet new console - создать новый проект
 // dotnet run - запустить проект
+
+//работа с gitHub
+//PS C:\Users\Admin\Desktop\ДЗ ГБ\C#\Examples> git init
+//PS C:\Users\Admin\Desktop\ДЗ ГБ\C#\Examples> git add Example001_HelloConsole
+//PS C:\Users\Admin\Desktop\ДЗ ГБ\C#\Examples> git commit -m "Add example001 hello!"
+//PS C:\Users\Admin\Desktop\ДЗ ГБ\C#\Examples> git branch -M main
+//PS C:\Users\Admin\Desktop\ДЗ ГБ\C#\Examples> git remote add origin https://github.com/Sistomin/lesson1.git
+//PS C:\Users\Admin\Desktop\ДЗ ГБ\C#\Examples> git push origin main
